cd713e7ca291076559cf0edf6979fe1b2111b1a9
Ijlal Hussain <ijlal.hussain@veroke.com>
2026-08-06 23:44:10 +0500

    Manage DB lifecycle with app state
    Convert TruckyApp to StatefulWidget and add WidgetsBindingObserver to close the database when the app is paused and reopen it on resume. Also closes the database on widget disposal.

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trucky/core/constants/app_constants.dart';
+import 'package:trucky/core/database/app_database.dart';
 import 'package:trucky/core/router/app_router.dart';
 import 'package:trucky/core/theme/app_theme.dart';
 import 'package:trucky/presentation/client_supplier/bloc/client_supp_bloc.dart';
@@ -12,8 +13,38 @@ import 'package:trucky/presentation/settings/bloc/settings_bloc.dart';
 import 'package:trucky/presentation/treasury/bloc/treasury_bloc.dart';
 
 /// Root application widget.
-class TruckyApp extends StatelessWidget {
+class TruckyApp extends StatefulWidget {
   const TruckyApp({super.key});
+
+  @override
+  State<TruckyApp> createState() => _TruckyAppState();
+}
+
+class _TruckyAppState extends State<TruckyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    AppDatabase.instance.close();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      // App is minimized, close the database.
+      AppDatabase.instance.close();
+    } else if (state == AppLifecycleState.resumed) {
+      // App is reopened, ensure the database is open again.
+      AppDatabase.instance.database;
+    }
+    super.didChangeAppLifecycleState(state);
+  }
 
   @override
   Widget build(BuildContext context) {
